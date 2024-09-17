ffbdfe2cfa86b5912d0e887f6808e28c6144ba99
Степан Рудаков <19stepan94@mail.ru>
2024-09-17 19:13:23 +0500

    при нажатии на кнопку удалить - удаляется задача, при нажатии на завершить - задача перечеркивается

--- src/modules/render.js
+++ src/modules/render.js
@@ -33,6 +33,46 @@ export const renderTodo = (app) => {
     form.reset();
   });
 
+  tbody.addEventListener('click', (e) => {
+    const target = e.target;
+
+   if(target.classList.contains('btn-danger')) {
+      const row = target.closest('tr');
+      row.remove();
+
+      Array.from(tbody.children).forEach((row, index) => {
+        row.children[0].textContent = index + 1;
+      });
+    }
+  });
+  
+  
+  tbody.addEventListener('click', (e) => {
+    const target = e.target;
+
+   if(target.classList.contains('btn-success')) {
+
+      const row = target.closest('tr');
+      const taskCell = row.querySelector('.task');
+      const statusCell = row.children[2];
+
+      taskCell.classList.toggle('text-decoration-line-through');
+      if (statusCell.textContent === 'В процессе') {
+        statusCell.textContent = 'Выполнена';
+        row.classList.remove('table-light');
+        row.classList.add('table-success');
+      } else {
+        statusCell.textContent = 'В процессе';
+        row.classList.remove('table-success');
+        row.classList.add('table-light');
+      }
+    }
+  });
+  
+
+
+
+
   app.append(title, form, table);
 
   return {
